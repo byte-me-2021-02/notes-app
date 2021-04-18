929468e8b0dec88df2186e1546a81f71dbe53651
brian <riceb53@gmail.com>
2021-04-18 11:38:18 -0400

    pass props down and events up

--- src/components/NoteSelector.vue
+++ src/components/NoteSelector.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="note-selector">
+  <div 
+    class="note-selector" 
+    v-bind:class="{active: note.id === selectedNoteId}"
+    v-on:click="selectNote(note)"
+  >
     <p class="note-selector-title">{{note.body}}</p>
     <p class="note-selector-timestamp">{{note.timestamp}}</p>
   </div>
@@ -7,6 +11,12 @@
 
 <script> 
 export default {
-  props: ["note"]
+  props: ["note", "selectedNoteId"],
+  methods: {
+    selectNote: function(note) {
+      // this.selectedNoteId = note.id;
+      this.$emit("selectNote", note);
+    }
+  }
 }
 </script>
--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="note-selectors">
-    <NoteSelector v-for="note in notes" v-bind:note="note" />
+    <NoteSelector 
+      v-for="note in notes" 
+      v-bind:note="note" 
+      v-bind:key="note.id" 
+      v-bind:selectedNoteId="selectedNoteId" 
+      v-on:selectNote="selectNote"
+    />
   </div>
 </template>
 
@@ -14,7 +20,13 @@ export default {
         { id: 2, body: "This is a second test", timestamp: Date.now() },
         { id: 3, body: "This is a third test", timestamp: Date.now() },
         { id: 4, body: "This is a fourth test", timestamp: Date.now() }
-      ]
+      ],
+      selectedNoteId: 2
+    }
+  },
+  methods: {
+    selectNote: function(note) {
+      this.selectedNoteId = note.id;
     }
   },
   components: {
